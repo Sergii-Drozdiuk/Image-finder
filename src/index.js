002fa31692978b9abefd35bede50698a1970fbf3
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-09-07 22:08:17 +0300

    infinity-scroll

--- src/index.js
+++ src/index.js
@@ -2,20 +2,54 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages, imgPerPage } from "../src/js/fetch"
+import throttle from 'lodash.throttle';
 
 const form = document.getElementById('search-form');
+const input = document.querySelector('input[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
+const checkBox = document.querySelector('.infinity-check');
+const guard = document.querySelector(".js-guard");
 const loadMoreBtn = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    });
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+const options = {
+  rootMargin: "300px",
+};
+const observer = new IntersectionObserver(handlerLoadMore, options);
+const STORAGE_KEY = "form-state";
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
+let page = 0;
+let searchQuery = '';
+let numberOfPages = 0;
+
+if (savedData !== {}) {
+  input.value = savedData.searchQuery || '';
+};
+
+form.addEventListener('input', throttle(saveInputValue, 500));
 form.addEventListener('submit', handleSubmit);
 loadMoreBtn.addEventListener('click', loadMoreImages);
+checkBox.addEventListener('change', InfiniteScroll);
 
-let page = 1;
-let searchQuery = '';
+InfiniteScroll()
+
+function saveInputValue(evt) {
+  const formData = savedData;
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function InfiniteScroll() {
+  if (checkBox.checked) {
+    observer.observe(guard);
+    hideBtn();
+  } else {
+    observer.unobserve(guard);
+  }
+};
 
 function handleSubmit(evt) {
   evt.preventDefault();
@@ -23,20 +57,36 @@ function handleSubmit(evt) {
   page = 1;
   clearGallery();
   fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
-}
+};
 
 function loadMoreImages() {
   page += 1;
   fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
   scrollPage();
-}
+  loadMoreBtn.blur();
+};
+
+function handlerLoadMore(entries) {
+  if (page === 0 || page === numberOfPages) {
+    return;
+  }
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
+      scrollPage();
+    };
+  });
+};
 
 const errorMessage = () => Notify.failure('Sorry, something went wrong. Please try again.');
 
 function checkNumberOfPages(resp) {
   const { totalHits, hits } = resp
-  let numberOfPages = Math.ceil(totalHits / imgPerPage);
-  
+  numberOfPages = Math.ceil(totalHits / imgPerPage);
+
+  if (!checkBox.checked || page !== numberOfPages) { showBtn() };
+ 
   if (totalHits === 0) {
     Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     return form.reset();
@@ -47,12 +97,13 @@ function checkNumberOfPages(resp) {
   }
   
   if (page === numberOfPages) {
+    observer.unobserve(guard);
     hideBtn();
     Notify.info("We're sorry, but you've reached the end of search results.");
-  } else { showBtn() };
+  };
 
   renderImages(hits);
-}
+};
 
 function renderImages(hits) {
   const imageMarkup = hits
@@ -75,12 +126,12 @@ function renderImages(hits) {
     .join('');
   gallery.insertAdjacentHTML('beforeend', imageMarkup);
   lightbox.refresh();
-}
+};
 
 function clearGallery() {
   gallery.innerHTML = '';
   hideBtn();
-}
+};
 
 function scrollPage() {
   const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
@@ -88,14 +139,12 @@ function scrollPage() {
     top: cardHeight,
     behavior: 'smooth',
   });
-}
+};
 
 function hideBtn() {
-  loadMoreBtn.classList.remove('is-visible');
-  loadMoreBtn.classList.add('is-hidden');
-}
+  loadMoreBtn.classList.replace('is-visible', 'is-hidden');
+};
 
 function showBtn() {
-  loadMoreBtn.classList.remove('is-hidden');
-  loadMoreBtn.classList.add('is-visible');
-}
+  loadMoreBtn.classList.replace('is-hidden', 'is-visible');
+};
