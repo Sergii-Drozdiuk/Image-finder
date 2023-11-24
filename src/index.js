ba97db61a087150c1d0b4d3a21a9e36436dcd861
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-11-24 21:27:41 +0200

    fix-bugs

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages, imgPerPage } from "../src/js/fetch"
+import { fetchImages, imgPerPage } from '../src/js/fetch';
 import throttle from 'lodash.throttle';
 
 const form = document.getElementById('search-form');
 const input = document.querySelector('input[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
 const checkBox = document.querySelector('.infinity-check');
-const guard = document.querySelector(".js-guard");
+const guard = document.querySelector('.js-guard');
 const loadMoreBtn = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 const options = {
-  rootMargin: "300px",
+  rootMargin: '300px',
 };
 const observer = new IntersectionObserver(handlerLoadMore, options);
-const STORAGE_KEY = "form-state";
+const STORAGE_KEY = 'form-state';
 const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 let page = 0;
 let searchQuery = '';
 let numberOfPages = 0;
 
-if (savedData !== {}) {
+if (savedData) {
   input.value = savedData.searchQuery || '';
-};
+}
 
 form.addEventListener('input', throttle(saveInputValue, 500));
 form.addEventListener('submit', handleSubmit);
 loadMoreBtn.addEventListener('click', loadMoreImages);
 checkBox.addEventListener('change', InfiniteScroll);
 
-InfiniteScroll()
+InfiniteScroll();
 
 function saveInputValue(evt) {
   const formData = savedData;
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-};
+}
 
 function InfiniteScroll() {
   if (checkBox.checked) {
@@ -49,7 +49,7 @@ function InfiniteScroll() {
   } else {
     observer.unobserve(guard);
   }
-};
+}
 
 function handleSubmit(evt) {
   evt.preventDefault();
@@ -57,60 +57,74 @@ function handleSubmit(evt) {
   page = 1;
   clearGallery();
   fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
-};
+}
 
 function loadMoreImages() {
   page += 1;
   fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
   scrollPage();
   loadMoreBtn.blur();
-};
+}
 
 function handlerLoadMore(entries) {
   if (page === 0 || page === numberOfPages) {
     return;
   }
-  entries.forEach((entry) => {
+  entries.forEach(entry => {
     if (entry.isIntersecting) {
       page += 1;
-      fetchImages(searchQuery, page).then(checkNumberOfPages).catch(errorMessage);
+      fetchImages(searchQuery, page)
+        .then(checkNumberOfPages)
+        .catch(errorMessage);
       scrollPage();
-    };
+    }
   });
-};
+}
 
-const errorMessage = () => Notify.failure('Sorry, something went wrong. Please try again.');
+const errorMessage = () =>
+  Notify.failure('Sorry, something went wrong. Please try again.');
 
 function checkNumberOfPages(resp) {
-  const { totalHits, hits } = resp
+  const { totalHits, hits } = resp;
   numberOfPages = Math.ceil(totalHits / imgPerPage);
 
-  if (!checkBox.checked || page !== numberOfPages) { showBtn() };
- 
+  if (!checkBox.checked || page !== numberOfPages) {
+    showBtn();
+  }
+
   if (totalHits === 0) {
-    Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
     return form.reset();
   }
-  
+
   if (page === 1) {
     Notify.success(`Hooray! We found ${totalHits} images.`);
   }
-  
+
   if (page === numberOfPages) {
     observer.unobserve(guard);
     hideBtn();
     Notify.info("We're sorry, but you've reached the end of search results.");
-  };
+  }
 
   renderImages(hits);
-};
+}
 
 function renderImages(hits) {
   const imageMarkup = hits
-    .map( 
-      hit => {
-        const { largeImageURL, webformatURL, tags, likes, views, comments, downloads } = hit;
-        return `
+    .map(hit => {
+      const {
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = hit;
+      return `
         <a href="${largeImageURL}" class="photo-card-a">
           <div class="photo-card">
           <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
@@ -121,30 +135,31 @@ function renderImages(hits) {
           <p class="info-item"><b>Downloads:</b> ${downloads}</p>
         </div>
         </div>
-        </a>`
-      })
+        </a>`;
+    })
     .join('');
   gallery.insertAdjacentHTML('beforeend', imageMarkup);
   lightbox.refresh();
-};
+}
 
 function clearGallery() {
   gallery.innerHTML = '';
   hideBtn();
-};
+}
 
 function scrollPage() {
-  const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
   window.scrollBy({
     top: cardHeight,
     behavior: 'smooth',
   });
-};
+}
 
 function hideBtn() {
   loadMoreBtn.classList.replace('is-visible', 'is-hidden');
-};
+}
 
 function showBtn() {
   loadMoreBtn.classList.replace('is-hidden', 'is-visible');
-};
+}
